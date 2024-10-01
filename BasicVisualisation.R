8159f7116cfe83648d2b025342a5b13a9426248a
Kalicz Péter <kaliczp@gmail.com>
2024-10-01 16:26:20 +0200

    Object creating and listing

--- BasicVisualisation.R
+++ BasicVisualisation.R
@@ -1,2 +1,15 @@
 ## Generate random data
 rnorm(50)
+## Optional form
+rnorm(n = 50)
+rnorm(50, mean = 0, sd = 1)
+## Assign to a variable
+a <- rnorm(50)
+## List all the saved objects
+ls()
+## You can use any other arbitary name (some rules in naming)
+mysecondobject <- rnorm(100)
+## You can use equal sign too
+mythirdobject = rnorm(80)
+## The syste CASE SENSITIVE
+MyFourthObject <- rnorm(70)
