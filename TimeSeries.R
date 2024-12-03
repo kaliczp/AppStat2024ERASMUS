8c1542c9cf662f70ddb8a02df8ff9b656a14679e
Kalicz Péter <kaliczp@gmail.com>
2024-12-03 16:19:22 +0100

    Advanced plot

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,9 +1,18 @@
 ## Time series intro
 temp <- read.table("ta_h_Sopron_18702023.csv", sep = ";", head = TRUE)
+## Sipmle plot against index
 plot(temp$ta, type = "l")
+## Date introduced
 Sys.Date()
 Sys.Date() + 1 # tomorrow
 as.Date("1870-01-01")
 tempDate <- as.Date("1870-01-01") + 0:(nrow(temp)-1)
 tempDate <- as.Date("1869-12-31") + 1:nrow(temp)
+## Plot wiht date
 plot(tempDate, temp$ta, type = "l")
+## Customised time axis with advanced plot
+plot(tempDate, temp$ta, type = "l", xaxs = "i", xaxt = "n", xlab = "", ylab = "Temperature")
+## Creat yearly tickas.Date("1870-01-01")
+as.Date("1870-01-01") + c(0,365, 2:5*365) # wrong because leap year
+beginyear <- as.Date(paste0(1870:2023, "-01-01"))
+axis.Date(side = 1, at = beginyear)
